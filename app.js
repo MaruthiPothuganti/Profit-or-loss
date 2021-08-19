9071d7473665237fdcdb33052f08d7492804943d
Maruthi Pothuganti <maruthicharypothuganti@gmail.com>
2021-08-19 14:54:05 +0530

    chore and style

--- app.js
+++ app.js
@@ -5,27 +5,48 @@ const currentStockPrice = document.querySelector("#input3");
 const btnCalculate = document.querySelector("#btn-Check");
 
 const output = document.querySelector("#output");
+output.style.display = "none";
+document.getElementById('loading').style.display = "none";
+document.getElementById('profit').style.display = "none";
+document.getElementById('loss').style.display = "none";
 
 function showOutputMessage(msg) {
     output.innerText = msg;
+    output.style.display = "";
 
 }
 btnCalculate.addEventListener("click", function calculate() {
     var currentCost = Number(currentStockPrice.value) * Number(numberOfStocks.value);
     var initialCost = Number(initialStockPrice.value) * Number(numberOfStocks.value);
+    document.getElementById('loading').style.display = "none";
+    document.getElementById('profit').style.display = "none";
+    document.getElementById('loss').style.display = "none";
+    output.style.display = "none";
+
 
     if (Number(currentStockPrice.value) > Number(initialStockPrice.value)) {
         var loss = currentCost - initialCost;
         var lossPer = (loss / initialCost) * 100;
-        output.style.color = "red";
-        showOutputMessage("The loss is ₹ " + loss + ". The loss percentage is " + lossPer + "% 😔");
-
+        document.getElementById('loading').style.display = "block";
+        setTimeout(function() {
+            document.getElementById('loading').style.display = "none";
+            output.style.color = "red";
+            showOutputMessage("The loss is ₹ " + loss + ". The loss percentage is " + lossPer + "% 😔");
+            document.getElementById('loss').style.display = "block";
+        }, 5000)
 
     } else if (Number(initialStockPrice.value) > Number(currentStockPrice.value)) {
         var profit = initialCost - currentCost;
         var profitPer = (profit / initialCost) * 100;
-        output.style.color = "green";
-        showOutputMessage("The profit is ₹ " + profit + ". The profit percentage is " + profitPer + "% 🤑🥳");
+        document.getElementById('loading').style.display = "block";
+        setTimeout(function() {
+            document.getElementById('loading').style.display = "none";
+            output.style.color = "green";
+            showOutputMessage("The profit is ₹ " + profit + ". The profit percentage is " + profitPer + "% 🤑🥳");
+            document.getElementById('profit').style.display = "block";
+        }, 5000)
+
+
 
     } else {
         showOutputMessage("No pain, No gain");
